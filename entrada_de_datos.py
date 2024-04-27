d9da2af1694261314a37fd9b8dfd4b2deedd1f9a
Carmen <tenoriocarmen243@gmail.com>
2024-04-26 20:49:57 -0400

    operadores de asignacion y comparacion, entrada de datos

---
+++ entrada_de_datos.py
@@ -0,0 +1,12 @@
+mi_variable = input('Escribe tu valor: ')
+print('tu valor es: '+ mi_variable)
+print(type(mi_variable))
+
+a= input('Primer numero: ')
+a_transformado=int(a)
+b = input('Segundo numero: ')
+b=int(b)
+c= a_transformado + b 
+print('el resultado es ' + str(c))
+
+
---
+++ operadores.py
@@ -0,0 +1,25 @@
+'''operadores
+% modulo
+** exponente
+// division entera
+'''
+print(17%3)
+print(3**2)
+print(24//9)
+print('\n')
+a=5
+b=8
+a+=b
+print(a)
+print('\n')
+a1=3
+b1=5
+a1*=b1
+print(a1)
+
+print( 5== 5 )
+print( 5!= 5 )
+print( 5 > 8 )
+print( 5 < 8 )
+print( 3 <= 10 )
+print( 3 >= 10 )
